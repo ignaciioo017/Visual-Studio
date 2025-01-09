aa6a11888bed42b9639470f94b61e08a8aa49665
Ignaciioo017 <igraca05@gmail.com>
2025-01-09 12:19:52 +0100

    actu

--- bases/src/app/dbz/pages/main.page.component.ts
+++ bases/src/app/dbz/pages/main.page.component.ts
@@ -1,3 +1,4 @@
+import { DbzService } from '../services/dbz.service';
 import { Character } from './../interfaces/character.interface';
 import { Component} from '@angular/core';
 
@@ -8,24 +9,9 @@ import { Component} from '@angular/core';
 })
 
 export class MainPageComponent {
-//Lista con los personajes predeterminados
-  public characters: Character[] = [{
-    name: 'Krillin',
-    power: 1000
-  },{
-    name:'Goku',
-    power: 9500
-  },{
-    name:"Vegeta",
-    power: 7500
-  }];
+  
+  constructor( public dbzService:DbzService ){
 
-//metodos para agregar y eliminar los personajes
-  onNewCharacter(character:Character): void{
-    this.characters.push(character);
-  }
-  onDeleteCharacter(index:number){
-    this.characters.splice(index,1);
   }
 
 }

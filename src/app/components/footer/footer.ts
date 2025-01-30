d576b728d6a68658caa2f1364e2f5ec49e4f1ea2
Ignaciioo017 <igraca05@gmail.com>
2025-01-30 13:13:41 +0100

    Estructura

---
+++ src/app/components/footer/footer.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'selector-name',
+    templateUrl: 'footer.html'
+})
+
+export class NameComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
---
+++ src/app/components/header/header.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'selector-name',
+    templateUrl: 'header.html'
+})
+
+export class NameComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
